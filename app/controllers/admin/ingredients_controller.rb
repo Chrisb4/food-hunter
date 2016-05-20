6247f75729432d1ce509eabfdeb0316372874bce
Haydeé Muñoz <haydeemunozdelarocha@gmail.com>
2016-05-20 15:15:51 -0500

    admin ingredient all controller actions

--- app/controllers/admin/ingredients_controller.rb
+++ app/controllers/admin/ingredients_controller.rb
@@ -1,8 +1,52 @@
 module Admin
-class IngredientsController < ActionController::Base
+  class IngredientsController < ActionController::Base
+    before_action :set_ingredient, only: [:edit, :update, :destroy]
 
-def index
-end
 
-end
+    def index
+      @ingredients=Ingredient.all
+    end
+
+    def new
+      @ingredient = Ingredient.new
+    end
+
+    def create
+      @ingredient = Ingredient.new(ingredient_params)
+      if @ingredient.save
+        redirect_to admin_ingredients_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @ingredient.update_attributes(ingredient_params)
+        redirect_to admin_ingredients_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @ingredient.destroy
+      redirect_to admin_ingredients_path
+    end
+
+private
+
+      def set_ingredient
+        @ingredient = Ingredient.find(params[:id])
+      end
+
+      def ingredient_params
+        params.require(:ingredient).permit(:name)
+      end
+    end
+
+
+  end
 end
