0f26cca1311ac049fe19d0342799d0e74f1e2c89
Laura Mateus <lmateus83@gmail.com>
2016-05-21 14:44:03 -0500

    listing all ingredients

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,22 +1,26 @@
 $( document ).ready(function() {
 
+// functions ready at page load
 getIngredients();
 
+// FUNCTIONS
 function getIngredients() {
 
-    var ingredients = $.ajax({
-      url: '/api/ingredients',
-      type: 'GET',
-      dataType: 'json',
-    });
+  var ingredients = $.ajax({
+    url: '/api/ingredients',
+    type: 'GET',
+    dataType: 'json',
+  });
 
-    ingredients.done(function(data){
-      $('.ingredients').html('<p>' + data[1].name + '</p>');
-    });
+  ingredients.done(function(data){
+    for (var i = 0; i < data.length; i++) {
+      $('.ingredients').append('<p>' + data[i].name + '</p>');
+    }
+  });
 
-    ingredients.fail(function(jqXHR, textStatus, errorThrown){
-      console.log(errorThrown);
-    });
-  }
+  ingredients.fail(function(jqXHR, textStatus, errorThrown){
+    console.log(errorThrown);
+  });
+}
 
 })
