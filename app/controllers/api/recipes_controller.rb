c82d8588bcd33c372ef93a42ec00ed9f14b54dce
Laura Mateus <lmateus83@gmail.com>
2016-05-24 22:20:06 -0500

    gets only recipes that match checked ingredients

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -2,14 +2,22 @@ module Api
   class RecipesController < ApplicationController
     def index
       ingredients = params[:ingredients]
+      recipes = []
 
-      if ingredients
-        @recipes = Recipe.joins(:ingredients).where(ingredients: { id: ingredients }).includes(:ingredients)
-        else
-        @recipes = Recipe.all
+      Recipe.all.each do |recipe|
+        match = true
+
+        recipe.ingredients.each do |ingredient|
+          unless ingredients.include?(ingredient.id.to_s)
+            match = false
+            break
+          end
+        end
+
+        recipes << recipe if match
       end
 
-      render json: @recipes
+      render json: recipes
     end
   end
 end
