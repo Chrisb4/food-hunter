94fa6f3676a6014e8af7506d0dc0d1e46d14891b
Laura Mateus <lmateus83@gmail.com>
2016-05-28 14:14:05 -0500

    styled recipe card

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -81,9 +81,9 @@ $( document ).ready(function() {
       for (var i = 0; i < data.length; i++) {
         $('.recipes').append('<div class="recipe">' +
           '<div class="image" style="background-image: url(' + data[i].photo_url + '"></div>' +
-          '<h4 class="recipe-title">' + data[i].name + ' / ' + data[i].description + '</h4>' +
-          '<p class="cooking-time">' + 'Cooking Time: ' + data[i].minutes + ' minute(s)' +
-          '</p>' + '<p>' + data[i].directions + '</p>' + '</div>');
+          '<h4 class="recipe-title">' + data[i].name + '</h4>' +
+          '<p class="cooking-time">Cooking Time: ' + data[i].minutes + ' minute(s)</p>' +
+          '<p class="recipe-directions">Directions: ' + data[i].directions + '</p></div>');
       }
     });
 
