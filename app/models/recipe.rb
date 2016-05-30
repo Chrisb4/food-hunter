8da4dbbc15fd5994c08c5ccc81dfa98348191b3d
Laura Mateus <lmateus83@gmail.com>
2016-05-30 14:10:04 -0500

    adding all required pictures to seed file

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :ingredients
-  has_attached_file :photo, styles: { :medium => "200x", :thumb => "100x"}, :default_url => "assets/missing.png"
+  has_attached_file :photo, styles: { :medium => "200x", :thumb => "100x"}, :default_url => "missing.png"
   validates_attachment_content_type :photo, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   def attributes
